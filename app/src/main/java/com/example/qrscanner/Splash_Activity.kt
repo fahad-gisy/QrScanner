db3d336e84ebb3e227ee37348b6a52d465c38436
Fahad alqiay <dbnsop@gmail.com>
2022-01-06 19:40:25 +0300

    start with bottom nav after finished permissions

--- app/src/main/java/com/example/qrscanner/Splash_Activity.kt
+++ app/src/main/java/com/example/qrscanner/Splash_Activity.kt
@@ -2,22 +2,30 @@ package com.example.qrscanner
 
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.os.Looper
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 private const val CAMERA_REQUEST_CODE = 101 // نسوي كود للكاميرا او اي شي نريد السماح له3
 class Splash_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        checkPermissionNow()
+
+        Handler().postDelayed({
+            checkPermissionNow()
+        },3000)
+
 
     }
     private fun checkPermissionNow(){ //نطلب من التطبيق يسال عن السماح للمستخدم ب استخدام الكاميرا1
       if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED){
-
+          goToMainAct()
       }else{
           requestThePermission()
       }
@@ -28,21 +36,57 @@ class Splash_Activity : AppCompatActivity() {
            CAMERA_REQUEST_CODE)
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {//نرث هذه الداله لو الكود صحيح نحط الاكشن هنا
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {//4نرث هذه الداله لو الكود صحيح نحط الاكشن هنا
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if(requestCode == CAMERA_REQUEST_CODE ){
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){ // لو تمت الموافقة
+        if(requestCode == CAMERA_REQUEST_CODE ){//لو الكود ريكوست يساوي كود الكاميرا يعني يرجع ترو6
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){ //5 لو تمت الموافقة
                 goToMainAct()
-            }
+            } else if(isUserPermanentlyDenied()){//لو المستخدم رفض بشكل دائم7
+                  showGoToSettingDialog()
+
+            }else requestThePermission()
         }
     }
 
-    private fun goToMainAct(){
-
-            var intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
+    private fun showGoToSettingDialog(){//هنا المستخدم يختار الموافقة او لا
+        AlertDialog.Builder(this)
+            .setTitle("الموفقة على الكاميرا")
+            .setMessage("التطبيق يحتاج الوصول الى الكاميرا لكي يتم مسح الكود")
+            .setPositiveButton("موافق؟"){dialog, which ->
+             goToAppSetting() // لو المستخدم وافق نروح اعادادات التطبيق
+            }
+            .setNegativeButton("لا"){dialog,which ->
+                Toast.makeText(this,"لكي بتستطيع مسح الكود يجب عليك الموافة على استخدام الكاميرا",Toast.LENGTH_SHORT).show()
+                finish()// لو رفض المستخدم نعرض الرسالة ذي وننهي العملية
+            }.show()
     }
 
+    private fun goToAppSetting(){// الدالة هاذي تودينا للعدادات التطبيق
+     var intent = Intent(ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts("package",packageName,null))
+        intent.addCategory(Intent.CATEGORY_DEFAULT)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+private fun isUserPermanentlyDenied():Boolean{
+
+return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//8لو نسخة النظام مارشميلو او الي بعدة
+    shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA).not() // نلغي الطلب 9
+} else {
+     return false
+}
+
+}
+    private fun goToMainAct(){
+                //عند الموافقة نروح للهوم
+            startActivity(Intent(this,MainActivity::class.java))
+           finish()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        checkPermissionNow()
+    }
 
 }
